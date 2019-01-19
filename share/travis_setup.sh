775b485bf06a31441a15c3d06b167d632a55b616
DenJ0hn <46600843+DenJ0hn@users.noreply.github.com>
2019-01-19 16:22:00 +0100

    Update travis_setup.sh

--- share/travis_setup.sh
+++ share/travis_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -evx
 
-mkdir ~/.dashcore
+mkdir ~/.motioncore
 
 # safety check
-if [ ! -f ~/.dashcore/.dash.conf ]; then
-  cp share/dash.conf.example ~/.dashcore/dash.conf
+if [ ! -f ~/.motioncore/.motion.conf ]; then
+  cp share/motion.conf.example ~/.motioncore/motion.conf
 fi
